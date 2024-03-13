95fe15dff9be1a52316526c1ff54e062801312a7
DiegoDAW94 <daw13.2024@gmail.com>
2024-03-13 17:14:29 +0100

    Proyecto documentado

--- src/examen/Fecha.java
+++ src/examen/Fecha.java
@@ -1,8 +1,24 @@
 package examen;
+/**
+ * Paquete examen del proyecto del examen de segunda evaluacion
+ * 
+ * 
+ *@author Diego Somovilla daw13.2024@gmail.com
+ *@version 1.0
+ * **/
 
-public class Fecha {
+ public class Fecha {
 	public static boolean fechaValida;
-
+	/**
+	 * El método validarFecha comprueba si una Fecha a la que se le pasan 3 parámetros
+	 * tiene unos valores acordes a los que una fecha se refieren,
+	 * devolverá True o False dependiendo si la fecha es correcta.
+	 * 
+	 * @param anio = se le dará un entero que hace referencia al año de la Fecha
+	 * @param mes = se le dará un entero que hace referencia al mes de la Fecha
+	 * @param dia = se le dará un entero que hace referencia al día de la Fecha
+	 * @return devolvera un booleano true o false dependiendo del valor de fechaValida
+	 **/
 	public static boolean validarFecha(int anio, int mes, int dia) {
 		fechaValida = false;
 		if ((dia > 0) && (mes > 0) && (anio > 0)) {
